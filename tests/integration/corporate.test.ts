ecf365d45321e787ed17183e0ebbdc78d024e9b5
Zabdi0819 <zabdibvb19@gmail.com>
2025-04-23 17:53:43 -0600

    feat: :sparkles: Code adjustment: corporate.test
    Code adjustment: corporate.test. Rsponse modification

--- tests/integration/corporate.test.ts
+++ tests/integration/corporate.test.ts
@@ -1,6 +1,6 @@
 import request from 'supertest';
-import app from '../../src/server';
-import { createUser, createParking } from '../utils';
+import app from '../../src/app';
+import { createUser, createParking, resetDate, mockDate } from '../utils';
 import { UserType } from '../../src/entities/checkin.entity';
 import { ParkingType } from '../../src/entities/parking.entity';
 import { AppError } from '../../src/utils/error';
@@ -37,54 +37,13 @@ describe('Integration test for Corporate user', () => {
   });
 
   afterAll(async () => {
-    jest.useRealTimers();
     if (AppDataSource.isInitialized) {
       await AppDataSource.destroy();
     }
+    resetDate();
   });
 
   describe('POST /check-in', () => {
-    it('✅ Should allow CORPORATE user to check-in in private parking on weekdays', async () => {
-      const mockDate = new Date('2025-04-08T10:00:00'); // Tuesday
-      jest.spyOn(global.Date, 'now').mockImplementation(() => mockDate.getTime());
-    
-      const response = await request(app)
-        .post('/check-in')
-        .set('Authorization', `Bearer ${corporateToken}`)
-        .send({ 
-          parkingId: privateParkingId,
-          userType: UserType.CORPORATE,
-        });
-      
-      console.log(response.body);
-    
-      expect(response.status).toBe(200);
-      expect(response.body.message).toBe('Access granted');
-    
-      jest.spyOn(global.Date, 'now').mockRestore();
-    });
-    
-
-    it('❌ Should NOT allow CORPORATE user to check-in in private parking on weekends', async () => {
-      const mockDate = new Date('2025-04-06T10:00:00'); // Sunday
-      jest.spyOn(global.Date, 'now').mockImplementation(() => mockDate.getTime());
-
-      const response = await request(app)
-        .post('/check-in')
-        .set('Authorization', `Bearer ${corporateToken}`)
-        .send({
-          parkingId: privateParkingId,
-          userType: UserType.CORPORATE,
-        });
-
-      console.log(response.body);
-
-      expect(response.status).toBe(403);
-      expect(response.body.errorCode).toBe('ACCESS_DENIED');
-
-      jest.spyOn(global.Date, 'now').mockRestore();
-    });
-
     it('✅ Should allow CORPORATE user to check-in in public parking', async () => {
       const response = await request(app)
         .post('/check-in')
@@ -94,10 +53,46 @@ describe('Integration test for Corporate user', () => {
           userType: UserType.CORPORATE,
         });
       
-      console.log(response.body);
+      expect(response.status).toBe(200);
+      expect(response.body.status).toBe('success');
+      expect(response.body.message).toBe('Access granted');
+    });
+
+    it('✅ Should allow CORPORATE user to check-in in private parking on weekdays', async () => {
+      mockDate('2025-04-22T10:00:00'); // Tuesday
+      const response = await request(app)
+        .post('/check-in')
+        .set('Authorization', `Bearer ${corporateToken}`)
+        .send({ 
+          parkingId: privateParkingId,
+          userType: UserType.CORPORATE,
+        });
       
       expect(response.status).toBe(200);
+      expect(response.body.status).toBe('success');
       expect(response.body.message).toBe('Access granted');
+    
+      resetDate();
+    });
+    
+
+    it('❌ Should NOT allow CORPORATE user to check-in in private parking on weekends', async () => {
+      mockDate('2025-04-20T10:00:00'); // Sunday
+
+      const response = await request(app)
+        .post('/check-in')
+        .set('Authorization', `Bearer ${corporateToken}`)
+        .send({
+          parkingId: privateParkingId,
+          userType: UserType.CORPORATE,
+        });
+
+      expect(response.status).toBe(403);
+      expect(response.body.status).toBe('error');
+      expect(response.body.errorCode).toBe('ACCESS_DENIED');
+      expect(response.body.message).toBe('Private parkings are only available on weekdays');
+
+      resetDate();
     });
 
     it('❌ Should NOT allow CORPORATE user to check-in in courtesy parking', async () => {
@@ -109,10 +104,10 @@ describe('Integration test for Corporate user', () => {
           userType: UserType.CORPORATE,
         });
 
-      console.log(response.body);
-      
       expect(response.status).toBe(403);
+      expect(response.body.status).toBe('error');
       expect(response.body.errorCode).toBe('ACCESS_DENIED');
+      expect(response.body.message).toBe('Only visitors can enter courtesy parkings');
     });
   });
 });
