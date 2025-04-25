0d9f3bdb04313d12da685e6f2046bd712fe588a8
ZabR19 <zabdi.ramirez@lionintel.com>
2025-04-25 02:44:20 -0600

    feat: :sparkles: Update parking
    Update parking

--- tests/integration/parkings.test.ts
+++ tests/integration/parkings.test.ts
@@ -1,6 +1,6 @@
 import request from 'supertest';
 import app from '../../src/app';
-import { createUser, resetDate, initializeTestDB } from '../utils';
+import { createUser, createParking, resetDate, initializeTestDB } from '../utils';
 import { UserType } from '../../src/entities/checkin.entity';
 import { ParkingType } from '../../src/entities/parking.entity';
 import { AppError } from '../../src/utils/error';
@@ -9,6 +9,8 @@ import AppDataSource from '../../src/config/data-source';
 describe('Integration test for parkings creation', () => {
   let providerToken: string;
   let parkingName: string;
+  let updateParkingId: string;
+  let updateParkingName: string;
 
   beforeAll(async () => {
     await initializeTestDB();
@@ -23,6 +25,12 @@ describe('Integration test for parkings creation', () => {
     if (!providerToken) {
         throw new AppError('Failed to obtain authentication token', 400);
     }
+
+    // Create parking to update
+    updateParkingName = `updateParking-${Date.now()}`;
+    const updateParking = await createParking(updateParkingName, '123456789', 100, ParkingType.PRIVATE);
+    updateParkingId = updateParking.id;
+
   });
 
   afterAll(async () => {
@@ -48,8 +56,30 @@ describe('Integration test for parkings creation', () => {
       expect(response.body.status).toBe('success');
       expect(response.body.data).toHaveProperty('id');
       expect(response.body.data.name).toBe(parkingName);
+      expect(response.body.data.contact).toBe("3331234567");
       expect(response.body.data.spots).toBe(150);
       expect(response.body.data.parkingType).toBe(ParkingType.PUBLIC);
+      expect(response.body.data).toHaveProperty('createdAt');
+      expect(response.body.data).toHaveProperty('updatedAt');
+    });
+
+    it('✅ Should allow for updating of a parking', async () => {
+      const response = await request(app)
+          .patch(`/parkings/${updateParkingId}`)
+          .set('Authorization', `Bearer ${providerToken}`)
+          .send({
+            contact: "4499000000",
+            spots: 200
+          });
+
+      expect(response.status).toBe(200);
+      expect(response.body).toHaveProperty('id');
+      expect(response.body.name).toBe(updateParkingName);
+      expect(response.body.contact).toBe("4499000000");
+      expect(response.body.spots).toBe(200);
+      expect(response.body.parkingType).toBe(ParkingType.PRIVATE);
+      expect(response.body).toHaveProperty('createdAt');
+      expect(response.body).toHaveProperty('updatedAt');
     });
 
     it('❌ Should NOT allow the creation of a parking with a repeat name', async () => {
